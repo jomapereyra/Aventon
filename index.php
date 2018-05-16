4065c22ca3c5c49ae8fd95d749fb13ae0e647375
diego <dieguitoibarra@hotmail.com>
2018-05-16 10:45:17 -0300

    agreguela pagina principal  y el index y los archivos de bootstrap

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+	<title> index </title>
+	<meta charset="utf-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="estilo.css">
+
+</head>
+<body>
+	<header>
+		<div class="container-fluid">
+			 <h1>header</h1>
+		</div>
+	</header>
+	<div class="container color2">
+		<br>
+		<br>
+		<br>
+	</div>
+	<div class="container color2">
+		<form action="" class="form-horizontal">
+			<div class="form-group">
+				<label for="nombre" class="control-label col-md-2">Email:</label>
+				<div class="col-md-10">
+					<input class="form-control" type="email" name="" id="nombre" placeholder="Email">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="contraseña" class="control-label col-md-2"> Contraseña: </label>
+				<div class="col-md-10">
+					<input class="form-control" type="password" name="" id="contraseña" placeholder="Contraseña">
+				</div>
+			</div>
+			<br>
+			<div class="form-group">
+				<div class="col-md-2  col-xs-offset-7 col-md-offset-10">
+					<button class="btn btn-primary">Enviar</button>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-md-2 col-md-offset-2">
+					<button class="btn btn-primary">Registrarse</button>
+				</div>
+			</div>
+		</form>
+	</div>
+	<div class="container color2">
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>	
+	</div>
+	<footer>
+		<div class="container">
+			<h3> footer</h3>
+		</div>
+	</footer>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ paginaprincipal.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+	<title> index </title>
+	<meta charset="utf-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="estilo.css">
+
+</head>
+<body>
+<header>
+	<div class="container-fluid">
+		 <h1>header</h1>
+	</div>
+</header>
+<div class="">
+	<nav class="navbar navbar-default navbar-static-top ">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1">
+					<span class="sr-only">Menu</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse"  id="navbar-1">
+				<ul class="nav navbar-nav">
+					<li role="presentation " class=""><a href="#">Buscar</a> </li>
+					<li role="presentation"><a href="#">Mi Perfl</a> </li>
+					<li role="presentation"><a href="#">Crear Viaje</a> </li>
+					<li class="dropdown">
+    					<a class="dropdown-toggle" data-toggle="dropdown" role="button" href="#">
+        					Mis Viajes
+        					<span class="caret"></span>
+        				</a>
+    					<ul class="dropdown-menu">
+    						<li role="presentation" class="active"><a href="#">mis viajes creados</a> </li>
+    						<li role="presentation" class="active"><a href="#">mis viajes participados</a> </li>
+   				 		</ul>
+  					</li>
+					<li role="presentation"><a href="#">Notificaciones</a> </li>
+					<li role="presentation"><a href="paginaprincipal.php">Inicio</a> </li>	
+				</ul>
+			</div>
+		</div>
+	</nav>
+</div>
+<div class="container">
+	<section class="main row">
+		<article>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+			</p>
+		</article>
+	</section>	
+</div>
+
+<footer>
+	<div class="container">
+		<h3> footer</h3>
+	</div>
+</footer>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
